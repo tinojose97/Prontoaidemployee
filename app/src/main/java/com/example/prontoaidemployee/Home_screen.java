7be6affed90c5f7d629e16f93bbdc4671ec432dc
SHAUN <shaunritty@gmail.com>
2019-04-29 15:43:37 +0530

    upload

--- app/src/main/java/com/example/prontoaidemployee/Home_screen.java
+++ app/src/main/java/com/example/prontoaidemployee/Home_screen.java
@@ -1,11 +1,13 @@
 package com.example.prontoaidemployee;
 
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,8 +19,10 @@ import com.google.firebase.database.ValueEventListener;
 public class Home_screen extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef;
+    Button m;
     String wname,uname,noty_msg,cusname,cusnum,cusloc;
     private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -53,6 +57,7 @@ public class Home_screen extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText("Home");
+
                     return true;
                 case R.id.navigation_notification:
                     mTextMessage.setText(noty_msg);
@@ -73,9 +78,21 @@ public class Home_screen extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+     addListenerOnButton();
+
+}
+    public void addListenerOnButton() {
+        m = (Button) findViewById(R.id.docupload);
+        m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home_screen.this, Doc_Upload.class);
+                startActivity(intent);
+            }
+        });
 
 
 
     }
-
 }
+
